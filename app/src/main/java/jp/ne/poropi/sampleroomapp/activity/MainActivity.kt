99b9b43b40c7f2f1412eb1e72b5188b2a3aac1a1
poropi <poropi44@gmail.com>
2019-04-29 17:03:07 +0900

    first commit

---
+++ app/src/main/java/jp/ne/poropi/sampleroomapp/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package jp.ne.poropi.sampleroomapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jp.ne.poropi.sampleroomapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
